f3ca086bf1954e4c5f4149aad747903b8b8aafa9
joshbollman <42840887+joshbollman@users.noreply.github.com>
2018-10-01 11:13:39 -0500

    Add project files.

---
+++ WebSignalR/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSignalR
+{
+    public class Chat : Hub
+    {
+        public async Task JoinGroup(string groupName, string nick)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task LeaveGroup(string groupName, string nick)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task GroupMessage(string groupName, string nick, string message)
+        {
+            await Clients.Group(groupName).SendAsync("SendAll", nick, message);
+        }
+
+        public async Task SendAll(string nick, string message)
+        {
+            await Clients.All.SendAsync("SendAll", nick, message);
+        }
+    }
+}
